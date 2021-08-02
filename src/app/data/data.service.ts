652ea433c5d17fb507324d31b6da471746207a86
kote1999 <kotekirkitadze@gmail.com>
2021-08-03 03:47:19 +0400

    http posting in progress

---
+++ src/app/data/data.service.ts
@@ -0,0 +1,17 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { UserSettings } from './user-settings';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DataService {
+
+  constructor(private http: HttpClient) { }
+
+  postUserSettingsForm(userSettings: UserSettings): Observable<any> {
+    return this.http.post('https://putsreq.com/ZtUnhVSrV3Zyoea1EC2g', userSettings);
+  }
+
+}
